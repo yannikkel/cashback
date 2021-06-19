c66f4e82c25b283c22289b0b0c538bd090531e0f
yannikkel <yannikkel@yandex.ru>
2021-06-19 20:38:44 +0300

    java

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,7 +1,11 @@
 package ru.netology.service;
 
 public class CashbackHackService {
-    private final int boundary = 1000;
+    private final int boundary;
+
+    {
+        boundary = 1000;
+    }
 
     public int remain(int amount) {
         return boundary - amount % boundary;
